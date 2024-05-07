efcae358fb1f2cf2597f788a3ba2ec425464c83e
Nazar Kolachko <biokapt@gmail.com>
2024-05-07 18:21:37 +0300

    Add project files.

---
+++ 2STUD/Menu.cs
@@ -0,0 +1,52 @@
+﻿using Laba4Students;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4
+{
+    internal class Menu
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+
+
+            int choice;
+            do
+            {
+                Console.WriteLine("============================================");
+                Console.WriteLine("Для виконання завдань Колачка  введіть 1");
+                Console.WriteLine("Enter your name");
+                Console.WriteLine("Для виходу з програми введіть 0");
+                Console.WriteLine("============================================");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("=============================");
+                        Console.WriteLine("Виконую варіант Колачка ");
+                        Console.WriteLine("=============================");
+                        Kolachko.TupM();
+                        Console.WriteLine("=============================");
+                        break;
+                     //add your code....
+                    case 0:
+                        Console.WriteLine("Зараз завершимо, тільки натисніть будь ласка ще раз Enter");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Команда ``{0}'' не розпізнана. Зробіь, будь ласка, вибір із 1, 2, 3, 0.", choice);
+                        break;
+                }
+            } while (choice != 0);
+
+
+
+        }
+    }
+}
---
+++ 2STUD/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+using static Laba4Students.Kolachko ;
+using static Laba4Students.Kharchenko;
+
+namespace Laba4Students
+{
+    public class Kolachko
+    {
+        static List<Student> ReadData(StreamReader sr)
+        {
+            List<Student> students = new List<Student>();
+            try
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    string[] subs = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    string newStr = string.Join(" ", subs);
+                    students.Add(new Student(newStr));
+                    line = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                sr.Close();
+            }
+            return students;
+        }
+
+        static void RunMenu(List<Student> studs)
+        {
+            RunMenu11(studs.ToArray());
+        }
+
+        static void RunMenu11(Student[] students)
+        {
+            foreach (var student in students)
+            {
+                double avgMark = ((student.mathematicsMark + student.physicsMark + student.informaticsMark) / 3.0);
+                if (avgMark > 4.5)
+                {
+                    Console.WriteLine($"{student.surName} {student.firstName} {student.patronymic}: {student.physicsMark}");
+                }
+            }
+        }
+
+        public static void TupM()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            List<Student> students = ReadData(sr);
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            RunMenu(students);
+        }
+    }
+
+    struct Student
+        {
+            public string surName;
+            public string firstName;
+            public string patronymic;
+            public char sex;
+            public string dateOfBirth;
+            public char mathematicsMark;
+            public char physicsMark;
+            public char informaticsMark;
+            public int scholarship;
+
+            public Student(string lineWithAllData)
+            {
+                string[] data = lineWithAllData.Split();
+                surName = data[0];
+                firstName = data[1];
+                patronymic = data[2];
+                sex = data[3][0];
+                dateOfBirth = data[4];
+                mathematicsMark = data[5][0];
+                physicsMark = data[6][0];
+                informaticsMark = data[7][0];
+                scholarship = int.Parse(data[8]);
+            }
+        }
+    
+
+    public static class Kharchenko
+    {
+        // Enter your code here...
+    }
+}
+
+    
